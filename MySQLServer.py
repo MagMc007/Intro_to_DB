3c18184324ced796e85701514c8fb84ce7f38613
MagMc007 <mergashasho7@gmail.com>
2025-06-28 12:52:26 +0300

    Updated mmysqlserver.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,6 +1,5 @@
 import mysql.connector
 
-
 try:
     mydb = mysql.connector.connect(
         host = "localhost", username = "root",
@@ -10,12 +9,11 @@ try:
     mycursor = mydb.cursor()
 
     mycursor.execute(
-        "CREATE DATABASE alx_book_store"
+        "CREATE DATABASE IF NOT EXISTS alx_book_store"
     )
     print("Database 'alx_book_store' created successfully!")
 except mysql.connector.errors.InterfaceError:
     print("Failed to connect to MySQL.")
-
 finally:
     mycursor.close()
     mydb.close()
