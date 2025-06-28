141a29adf97d94026c73dabd52489f6afacdfcf7
MagMc007 <mergashasho7@gmail.com>
2025-06-28 12:50:22 +0300

    added mysqlserver.py

---
+++ MySQLServer.py
@@ -0,0 +1,21 @@
+import mysql.connector
+
+
+try:
+    mydb = mysql.connector.connect(
+        host = "localhost", username = "root",
+        password = "2025007diditb@d"
+    )
+
+    mycursor = mydb.cursor()
+
+    mycursor.execute(
+        "CREATE DATABASE alx_book_store"
+    )
+    print("Database 'alx_book_store' created successfully!")
+except mysql.connector.errors.InterfaceError:
+    print("Failed to connect to MySQL.")
+
+finally:
+    mycursor.close()
+    mydb.close()
